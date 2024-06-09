df2df755bab9b520d621d8b36ad9bcfa6c3ada7d
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-06-09 23:37:02 +0100

    form updated

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'  # Necessary for session management
